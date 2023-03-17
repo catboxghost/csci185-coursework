d5d2fd144f6fc876b313fccb90d912eb6a097b28
Catherine <turakron@gmail.com>
2023-03-17 12:00:09 -0400

    added quiz 02

---
+++ lectures/lecture15/01-style-property-demo/main.js
@@ -0,0 +1,41 @@
+
+function makeRed() {
+    // your code here...
+    console.log('Change background to red');
+    document.querySelector('#section1').style.backgroundColor = 'red';
+    document.querySelector('#section1').style.color = 'white';
+};
+
+function makeBlue() {
+    // your code here...
+    console.log('Change background to blue');
+    document.querySelector('#section2').style.backgroundColor = 'blue';
+    document.querySelector('#section2').style.color = 'white';
+};
+
+function makePink() {
+    // your code here...
+    console.log('Change background to pink');
+    document.querySelector('#section3').style.backgroundColor = 'pink';
+};
+
+function makeOrange() {
+    // your code here...
+    console.log('Change background to orange');
+    document.querySelector('#section4').style.backgroundColor = 'orange';
+};
+
+function clearScreen() {
+    // your code here...
+    console.log('clears all sections');
+    document.querySelector('#section1').style.backgroundColor = 'white';
+    document.querySelector('#section1').style.color = 'black';
+    document.querySelector('#section2').style.backgroundColor = 'white';
+    document.querySelector('#section2').style.color = 'black';
+    document.querySelector('#section3').style.backgroundColor = 'white';
+    document.querySelector('#section3').style.color = 'black';
+    document.querySelector('#section4').style.backgroundColor = 'white';
+    document.querySelector('#section4').style.color = 'black';
+    
+};
+
